e67f569e153a5556cd45c8fc1e26501243032fd6
Alexandru Constantin <alexandru.constantin@gmail.com>
2022-12-12 15:10:24 -0500

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Renovation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
