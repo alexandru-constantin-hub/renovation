ca654f360f508446d076cccff9b4d3250bc10610
Alexandru Constantin <aconstantin@zmartests.com>
2022-12-19 08:49:59 -0500

    Improve Announce Details, Mes Announce

--- Controllers/EspaceMembresController.cs
+++ Controllers/EspaceMembresController.cs
@@ -139,7 +139,88 @@ namespace RenovationFinale.Controllers
         }
 
 
-        
+
+        // GET: Announces/Details/5
+        public async Task<IActionResult> AnnounceDetails(int? id)
+        {
+            if (id == null || _context.Announces == null)
+            {
+                return NotFound();
+            }
+
+            var announce = await _context.Announces
+                .Include(a => a.IdDesactivateurNavigation)
+                .Include(a => a.IdPieceNavigation)
+                .Include(a => a.IdTypeRenovationNavigation)
+                .Include(a => a.IdUtilisateurNavigation)
+                .FirstOrDefaultAsync(m => m.IdAnnounce == id);
+            if (announce == null)
+            {
+                return NotFound();
+            }
+
+            return View(announce);
+        }
+
+
+        // GET: Announces/Edit/5
+        public async Task<IActionResult> AnnounceEdit(int? id)
+        {
+            if (id == null || _context.Announces == null)
+            {
+                return NotFound();
+            }
+
+            var announce = await _context.Announces.FindAsync(id);
+            if (announce == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdDesactivateur"] = new SelectList(_context.Administrateurs, "IdAdministrateur", "IdAdministrateur", announce.IdDesactivateur);
+            ViewData["IdPiece"] = new SelectList(_context.Typepieces, "IdPiece", "IdPiece", announce.IdPiece);
+            ViewData["IdTypeRenovation"] = new SelectList(_context.Typerenovations, "IdTypeRenovation", "IdTypeRenovation", announce.IdTypeRenovation);
+            ViewData["IdUtilisateur"] = new SelectList(_context.Utilisateurs, "IdUtilisateur", "IdUtilisateur", announce.IdUtilisateur);
+            return View(announce);
+        }
+
+        // POST: Announces/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AnnounceEdit(int id, [Bind("IdAnnounce,Adresse,Duree,Dimensions,AutreInformations,Etat,IdUtilisateur,IdPiece,IdTypeRenovation,IdDesactivateur")] Announce announce)
+        {
+            if (id != announce.IdAnnounce)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(announce);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnnounceExists(announce.IdAnnounce))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdDesactivateur"] = new SelectList(_context.Administrateurs, "IdAdministrateur", "IdAdministrateur", announce.IdDesactivateur);
+            ViewData["IdPiece"] = new SelectList(_context.Typepieces, "IdPiece", "IdPiece", announce.IdPiece);
+            ViewData["IdTypeRenovation"] = new SelectList(_context.Typerenovations, "IdTypeRenovation", "IdTypeRenovation", announce.IdTypeRenovation);
+            ViewData["IdUtilisateur"] = new SelectList(_context.Utilisateurs, "IdUtilisateur", "IdUtilisateur", announce.IdUtilisateur);
+            return View(announce);
+        }
 
 
 
