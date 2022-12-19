c6a5a784be383ae2bd834b8fa89f1d85f3148110
Alexandru Constantin <aconstantin@zmartests.com>
2022-12-19 17:12:37 -0500

    Made the search option and select for

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,3 +8,45 @@ setTimeout(function () {
     // Closing the alert
     $('.alert').alert('close');
 }, 2000);
+
+
+$(document).ready(function () {
+
+    $("#myInput").on("keyup", function () {
+
+        var value = $(this).val().toLowerCase();
+
+        $("#myTable tr").filter(function () {
+
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+
+        });
+
+    });
+
+
+    $("#piece").on("change", function () {
+
+        var value = $(this).val().toLowerCase();
+
+        $("#myTable tr").filter(function () {
+
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+
+        });
+
+    });
+
+    $("#renovation").on("change", function () {
+
+        var value = $(this).val();
+
+        $("#myTable tr").filter(function () {
+
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+
+        });
+
+    });
+
+});
